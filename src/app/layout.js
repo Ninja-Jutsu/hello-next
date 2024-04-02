f66e44bd6e9be66e4d82c21523e859da41a70cbe
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-03 06:35:24 +0800

    Add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         {children}
-
+        <footer>CopyRight Anything</footer>
       </body>
     </html>
   );
